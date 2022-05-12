f203e5eebf5d5a7cba05405a7e4c2bc86cbf75d0
BinarySoftware <mkmikolajek@icloud.com>
2022-05-12 21:23:52 +0200

    add docs

--- launchPad/ViewController.swift
+++ launchPad/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 import AVFoundation
 
+/// LaunchPad main and only view.
+///
+/// LaunchPad is a simple application, written just in training purposes. It may be useful potentially for
+/// artists and sound designers. It uses EDM soundpack from [freesounds.org](freesounds.org)
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayers: [AVAudioPlayer] = []
     
+    /// The content view in the application. Doesn't really serve any purpose, other than making the app look cool.
     private let contentView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 0.9)
@@ -21,6 +26,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         return view
     }()
 
+    /// Happens right after view had ended loading. It prepares the audio players and all the views.
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,6 +39,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         drawLaunchpadButtons()
     }
     
+    /// Makes the `ContentView` look good.
     func setupAutoLayout() {
         contentView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         contentView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
@@ -40,7 +47,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
     }
 
-    
+    /// Generates 16 launchpad buttons.
     func drawLaunchpadButtons(){
         let offset         = 10
         let btnsOnX        = 4
@@ -86,6 +93,11 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         }
     }
     
+    /// Happens after clicking the buton on launchpad.
+    ///
+    /// Plays the music as well as changes the appearance of the pressed button for as long as the sound plays.
+    ///
+    /// - Parameter sender: the clicked button.
     @objc func buttonSelected(sender: UIButton!){
         let btnColor    = UIColor(hue: CGFloat(sender.tag)/16.0, saturation: 1.0, brightness: 0.8, alpha: 0.8)
         let originalImg = sender.currentImage
